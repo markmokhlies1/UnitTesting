4e4641a6c6ecec975b27d0f522d95fff8c01e69e
mark mokhlies <markmokhlies15@gmail.com>
2025-03-28 20:43:11 +0200

    second

--- CloudCustomers/CloudCustomers.API/Models/Address.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CloudCustomers.API.Models
-{
-    public class Address
-    {
-        public string Street { get; set; }
-        public string City { get; set; }
-        public string ZipCode { get; set; }
-    }
-}
